7c0e60a7fd61cfcb9bff1585da155246432244f3
hamzassnb123 <hamzassnb123@gmail.com>
2025-12-01 11:34:40 +0100

    first week

--- week1/day1/Exercices.cs
+++ week1/day1/Exercices.cs
@@ -1,105 +1,117 @@
-﻿
-    // Exercice 1 : The Greeting
-// Console.WriteLine("Welcome to C# Programming!");
+     void SayHello(string name)
+    {
+        Console.WriteLine($"Hello, {name}!");
+    }
 
-    // Exercice 2 : Personal Info
-// string name = "Fatima Zahra";
-// int age = 21;
-
-// Console.WriteLine("My name is : "+ name + " and I'm  "+ age + " Years Old. "  );
-
-    // Exercice 3 : The Calculator
-
-// int num1 = 15;
-// int num2 = 5;
-// int sum = num1 + num2;
-// Console.WriteLine(sum);
-
-    // Exercice 4: The Bouncer (If/Else)
-// int userAge = 10;
-
-// if (userAge >= 18) {
-//     Console.WriteLine("Access Granted.");
-// }
-// else {
-//     Console.WriteLine("Access Denied.");
-// }
-
-    // Exercice 5: The Countdown (Loops)
-// int i = 10;
-
-// while (i >= 1)
-// {
-//     Console.WriteLine(i);
-//     i--;
-// }
-// Console.WriteLine("Liftoff");
-
-    // Exercice 6: The Repeater (Functions)
-// static void SayHello(string name ){
-//     Console.WriteLine("Hello," + name +"!");
-// }
-// SayHello("fatima zahra");
-// SayHello("souhail");
-// SayHello("hamza");
-
-    // Exercice 7 : Even or Odd?
-// for (int r =1;r <=10;r++)
-// {
-//     if(r % 2 == 0)
-//     {
-//         Console.WriteLine("this numb "+r+ "is even");
-//     }
-//     else
-//     {
-//         Console.WriteLine("this numb "+r+ " is odd");
-//     }
-// }
-
-    // Exercice 8 : Temperature Converter
-//  Console.Write("Enter tempature in celsuis:");
-//  string input = Console.ReadLine();
-//  double Celsius = Convert.ToDouble(input);
-//  double Fahrenheit= Celsius * 9/5 + 32;
-//  Console.WriteLine("Tempture in Fahrenheit " + Fahrenheit+ "°F" );
-
-    // Exercice 9 : Number Swapper
-
-//  int a=10; 
-//  int b=30; 
-//  int z; 
-//  z=a;
-//  a=b;
-//  b=z;
-// //  Console.WriteLine( z);
-//  Console.WriteLine($"{a}, {z}, {b}");
-// //  Console.WriteLine( a);
-
-    // Exercice 10 : Simple Multiplication Table
- Console.Write("Enter number:");
- string x= Console.ReadLine();
- int number= int.Parse(x);
- for (int e=1; e<=10; e++)
- {
-     int result=e*number;
-     Console.WriteLine($"{e} * {number} = {result}");
- }
+ 
+      
+        Console.WriteLine("## Exercise 1");
+        Console.WriteLine("Welcome to C# Programming!");
+        Console.WriteLine("\n");
 
 
-// ----------------------------------------------------------------
+       
+        Console.WriteLine("## Exercise 2");
+        string name = "ayat mohamed"; 
+        int age = 21;           
 
-// Console.Write("Enter a string");
-// string nom = Console.ReadLine(); 
-// // Console.WriteLine(nom); 
-// string result = "";
+        Console.WriteLine($"My name is {name} and I am {age} years old.");
+        Console.WriteLine("\n"); 
 
-// for (int i =0;i < nom.Length; i++)
-// {
-//     if ()
-// }
+
+        Console.WriteLine("## Exercise 3");
+        int num1 = 45;
+        int num2 = 12;
+
+        int sum = num1 + num2;
+
+        Console.WriteLine($"The result of {num1} + {num2} is: {sum}");
+        Console.WriteLine("\n");
+
+
+     
+        Console.WriteLine("## Exercise 4");
+        
+        int userAge =15;
+
+        if (userAge >= 18)
+        {
+            Console.WriteLine("access granted");
+        }
+        else 
+        {
+            Console.WriteLine("access denied");
+        }
+        Console.WriteLine("\n");
+
+
+    
+        Console.WriteLine("#ex 5");
+        int counter =10;
+        while (counter >=1)
+
+            {
+                Console.WriteLine(counter);
+                counter--;
+            }
+    
+                Console.WriteLine("Liftoff!!");
+                Console.WriteLine("\n");
+
+
+            Console.WriteLine("ex 6");
+            SayHello ("ayat");
+            SayHello ("mohamed");
+            SayHello ("miliodas");
+            
+
+          Console.WriteLine("ex 7");
+            for (int i=1 ; i<=10 ; i++)
+        {
+            if (i % 2 == 0)
+            {
+                Console.WriteLine($"{i} is even");
+            }
+            else
+            {
+                Console.WriteLine($"{i} is odd");
+            }
+        }
+
+Console.WriteLine("Ex 8");
+Console.Write("Enter temperature in Celsius: ");
+
+double celsius = double.Parse(Console.ReadLine());
+double fahrenheit = celsius * 9.0 / 5.0 + 32;
+
+Console.WriteLine($"{celsius}°C = {fahrenheit}°F");
+Console.WriteLine();
 
 
 
+Console.WriteLine("Ex 9");
+
+int a = 10;
+int b = 20;
+
+Console.WriteLine($"Before swap: a = {a}, b = {b}");
+
+int temp = a;
+a = b;
+b = temp;
+
+Console.WriteLine($"After swap: a = {a}, b = {b}");
+Console.WriteLine();
 
 
+Console.WriteLine("Ex 10");
 
+Console.Write("Enter a number: ");
+
+int number = int.Parse(Console.ReadLine());
+
+for (int i = 1; i <= 10; i++)
+{
+    Console.WriteLine($"{number} x {i} = {number * i}");
+}
+Console.WriteLine();
--- week1/day1/FizzBuzz.cs
+++ week1/day1/FizzBuzz.cs
@@ -1,40 +1,35 @@
-    
-    // Exercise:FizzBuzz
+  Console.WriteLine("## exercise:FizzBuzz ");
 
-// Console.Write("Enter a number between 1 and 100: ");
-// int num = int.Parse(Console.ReadLine());
+Console.Write("Enter a number between 1 and 100: ");
+int num = int.Parse(Console.ReadLine());
 
-// if (num % 3 == 0 && num % 5 == 0)
-// {
-//     Console.WriteLine("FizzBuzz");
-// }
-// else if (num % 3 == 0)
-// {
-//     Console.WriteLine("Fizz");
-// }
-// else if (num % 5 == 0)
-// {
-//     Console.WriteLine("Buzz");
-// }
-// else
-// {
-//     Console.WriteLine("ereur");
-// }
-
-
-    // Exercise :Build a Triangle Pattern
-
-// for (int i = 1; i <= 5; i++)
-// {
-//     for(int j=1;j <= i; j++)
-//     {
-//         Console.Write("*");
-//     }
-//     Console.WriteLine();
-// }
+if (num % 3 == 0 && num % 5 == 0)
+{
+    Console.WriteLine("FizzBuzz");
+}
+else if (num % 3 == 0)
+{
+    Console.WriteLine("Fizz");
+}
+else if (num % 5 == 0)
+{
+    Console.WriteLine("Buzz");
+}
+else
+{
+    Console.WriteLine("ereur");
+}
 
 
-    // Exercise : Reverse a Word 
+ 
+for (int i = 1; i <= 5; i++)
+{
+    Console.WriteLine(new string('*', i));
+}
+
+
+Console.WriteLine("Exercise :Reverse a Word ");
+
 
 Console.Write("Enter a word: ");
 string word = Console.ReadLine();
